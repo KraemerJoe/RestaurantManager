ac1fe325e3f21a3fe5eabc56a70dd630c0da0444
Joe <joe.kraemer57@gmail.com>
2021-05-03 17:53:22 +0800

    build (root+dependency):joekraemer: ebean init
    initialisation of ebean connection

--- RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/RestaurantManager.java
+++ RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/RestaurantManager.java
@@ -1,9 +1,28 @@
 package fr.ul.miage.gl.restaurant;
 
+import fr.ul.miage.gl.restaurant.ebean.EbeanManager;
+
 public class RestaurantManager {
 
 	public static void main(String[] args) {
-		System.out.println("Restaurant Manager Sprint 1");
+		System.out.println("Restaurant Manager Sprint 2");
+		initManagers();
+		createDBConnection();
+		
+		if(EbeanManager.getInstance().getDb() != null) {
+			System.out.println("DB Connected !");
+		}else {
+			System.err.println("DB Not Connected, exit.");
+			System.exit(0);
+		}
+	}
+
+	private static void createDBConnection() {
+		EbeanManager.getInstance().initDB();
+	}
+
+	private static void initManagers() {
+		new EbeanManager();
 	}
 
 }
