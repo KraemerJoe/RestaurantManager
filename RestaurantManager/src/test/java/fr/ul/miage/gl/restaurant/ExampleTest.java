cee2e844a56abd4490aeab9c112d5b24bed077ab
Joe <joe.kraemer57@gmail.com>
2021-05-03 17:36:40 +0800

    Revert "build (root+dependency):joekraemer: adding ebean"
    This reverts commit 1a1b94b1e3e458f10d61bc9a949ca8ae8814a3b2.

---
+++ RestaurantManager/src/test/java/fr/ul/miage/gl/restaurant/ExampleTest.java
@@ -0,0 +1,13 @@
+package fr.ul.miage.gl.restaurant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Example")
+class TestExample {
+
+  
+}
