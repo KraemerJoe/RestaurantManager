f2177538c1f57300ac747962fff7d2a04c350e21
Joe <joe.kraemer57@gmail.com>
2021-05-04 18:13:32 +0800

    feat:(waiter):joekraemer:see table statut
    + switch string of table statut to enum
    + add function to permit the waiter to see his tables

--- RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/WaiterMenu.java
+++ RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/WaiterMenu.java
@@ -1,11 +1,19 @@
 package fr.ul.miage.gl.restaurant.menus.roles;
 
+import java.util.ArrayList;
+
+import fr.ul.miage.gl.restaurant.ebean.EbeanManager;
+import fr.ul.miage.gl.restaurant.managers.SessionManager;
+import fr.ul.miage.gl.restaurant.menus.ItemMenu;
 import fr.ul.miage.gl.restaurant.menus.Menu;
+import fr.ul.miage.gl.restaurant.pojo.dishes.RawMaterial;
+import fr.ul.miage.gl.restaurant.pojo.tables.TableAssignment;
+import fr.ul.miage.gl.restaurant.pojo.tables.TableRestaurant;
 
 public class WaiterMenu extends Menu {
 
 	public static WaiterMenu instance;
-	
+
 	public WaiterMenu() {
 		super("Waiter");
 		instance = this;
@@ -13,14 +21,25 @@ public class WaiterMenu extends Menu {
 
 	@Override
 	public void initMenuItems() {
-
+		itemList.add(new ItemMenu("Status of the restaurant", "See my tables"));
 	}
 
 	@Override
 	public void executeChoice(int choice) {
-
+		switch (choice) {
+		case 1:
+			ArrayList<TableAssignment> tables = new ArrayList<TableAssignment>();
+			
+			tables.addAll(EbeanManager.getInstance().getDb().find(TableAssignment.class)
+					.where()
+					.eq("staff_id", SessionManager.getInstance().getAccount().getStaff_id())
+					.findList());
+			for (TableAssignment ta : tables) {
+				System.out.println(ta.getTable().getColor() + "-[" + ta.getTable().getTable_id() + "] | Floor: " + ta.getTable().getFloor() + " | Seats: " + ta.getTable().getSeats_amount());
+			}
+		}
 	}
-	
+
 	public static WaiterMenu getInstance() {
 		if (instance == null)
 			instance = new WaiterMenu();
