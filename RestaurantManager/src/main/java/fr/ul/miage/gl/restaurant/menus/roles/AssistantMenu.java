fd02cb437ed86f6320f2464579c04dcf09be00a4
MDenis <melvyn.denis21@gmail.com>
2021-05-05 18:29:37 +0200

    feat:(assistant):melvyndenis: occupation restaurant
    + first choice for the assistant

--- RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/AssistantMenu.java
+++ RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/AssistantMenu.java
@@ -1,7 +1,13 @@
 package fr.ul.miage.gl.restaurant.menus.roles;
 
+import java.util.ArrayList;
+
+import fr.ul.miage.gl.restaurant.ebean.EbeanManager;
+import fr.ul.miage.gl.restaurant.managers.SessionManager;
+import fr.ul.miage.gl.restaurant.menus.ItemMenu;
 import fr.ul.miage.gl.restaurant.menus.Menu;
 import fr.ul.miage.gl.restaurant.menus.MenuLogin;
+import fr.ul.miage.gl.restaurant.pojo.tables.TableAssignment;
 
 public class AssistantMenu extends Menu {
 
@@ -14,12 +20,15 @@ public class AssistantMenu extends Menu {
 
 	@Override
 	public void initMenuItems() {
-
+		itemList.add(new ItemMenu("Occupation of the restaurant", "See the occupation"));
 	}
 
 	@Override
 	public void executeChoice(int choice) {
-
+		switch (choice) {
+		case 1:
+			System.out.println("test");
+		}
 	}
 	
 	public static AssistantMenu getInstance() {
