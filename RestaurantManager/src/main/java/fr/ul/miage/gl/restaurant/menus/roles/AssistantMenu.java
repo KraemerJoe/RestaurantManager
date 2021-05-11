037e1008713e6f37a4fc90eb20912c482f910ed8
MDenis <melvyn.denis21@gmail.com>
2021-05-11 14:35:06 +0200

    feat:(assistant):denismelvyn:visualize room occupation
    + option to visualize tables occupation

--- RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/AssistantMenu.java
+++ RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/AssistantMenu.java
@@ -11,6 +11,7 @@ import fr.ul.miage.gl.restaurant.pojo.orders.EnumOrderStat;
 import fr.ul.miage.gl.restaurant.pojo.orders.EnumSessionOrderStat;
 import fr.ul.miage.gl.restaurant.pojo.orders.Order;
 import fr.ul.miage.gl.restaurant.pojo.orders.SessionOrder;
+import fr.ul.miage.gl.restaurant.pojo.tables.TableRestaurant;
 
 public class AssistantMenu extends Menu {
 
@@ -25,6 +26,7 @@ public class AssistantMenu extends Menu {
 	public void initMenuItems() {
 		itemList.add(new ItemMenu("Take an order", "Create a new order for a client"));
 		itemList.add(new ItemMenu("Checkout", "See the evolution of the dishs of each table"));
+		itemList.add(new ItemMenu("Room Occupation", "See the occupation of the room"));
 	}
 
 	@Override
@@ -40,6 +42,14 @@ public class AssistantMenu extends Menu {
 			for (SessionOrder session : list) {
 				System.out.println("[TABLE #" + session.getOrder().getSessionClient().getTable_id().getTable_id() + "] " + session.getDish().getName() + " | " + session.getStatut());
 			}
+		case 3:
+			List<TableRestaurant> listTable = new ArrayList<TableRestaurant>();
+			listTable = EbeanManager.getInstance().getDb().find(TableRestaurant.class).orderBy().asc("table_id").findList();
+			for (TableRestaurant table : listTable) {
+				System.out.println("[TABLE #" + table.getTable_id() + "] " + table.getStatut());
+			}
+			
+			break;
 		default:
 			break;
 		}
