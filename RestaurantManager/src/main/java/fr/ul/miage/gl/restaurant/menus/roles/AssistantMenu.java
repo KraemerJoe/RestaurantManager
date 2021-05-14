25260a0d94b242f4ed5b967e2d8b481b39723fa9
MDenis <melvyn.denis21@gmail.com>
2021-05-14 23:20:12 +0200

    feat:(assistant):denismelvyn:check tables state
    + menu choice to check which tables are clean or need to be cleaned

--- RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/AssistantMenu.java
+++ RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/AssistantMenu.java
@@ -11,6 +11,7 @@ import fr.ul.miage.gl.restaurant.pojo.orders.EnumOrderStat;
 import fr.ul.miage.gl.restaurant.pojo.orders.EnumSessionOrderStat;
 import fr.ul.miage.gl.restaurant.pojo.orders.Order;
 import fr.ul.miage.gl.restaurant.pojo.orders.SessionOrder;
+import fr.ul.miage.gl.restaurant.pojo.tables.EnumTableStat;
 import fr.ul.miage.gl.restaurant.pojo.tables.TableRestaurant;
 
 public class AssistantMenu extends Menu {
@@ -27,6 +28,7 @@ public class AssistantMenu extends Menu {
 		itemList.add(new ItemMenu("Take an order", "Create a new order for a client"));
 		itemList.add(new ItemMenu("Checkout", "See the evolution of the dishs of each table"));
 		itemList.add(new ItemMenu("Room Occupation", "See the occupation of the room"));
+		itemList.add(new ItemMenu("Tables state", "See if tables are clean or not"));
 	}
 
 	@Override
@@ -50,6 +52,21 @@ public class AssistantMenu extends Menu {
 			}
 			
 			break;
+		case 4:
+			List<TableRestaurant> listTableClean = new ArrayList<TableRestaurant>();
+			listTable = EbeanManager.getInstance().getDb().find(TableRestaurant.class).orderBy().asc("floor").orderBy().asc("table_id").findList();
+			String tableState="";
+			for (TableRestaurant table : listTable) {
+				if(table.getStatut().equals(EnumTableStat.TO_CLEAN)) {
+					tableState="Dirty";
+				}else {
+					tableState="Clean";
+				}
+				System.out.println("[TABLE #" + table.getTable_id() + "] Floor : " + table.getFloor() + " | Table state : " + tableState);
+			}
+			
+			break;
+
 		default:
 			break;
 		}
