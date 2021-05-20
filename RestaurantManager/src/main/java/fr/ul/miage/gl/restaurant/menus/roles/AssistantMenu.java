c9f6ab7ba4266a8dbceae3c9824e132f1db382fb
Felix <Fel.dupt@gmail.com>
2021-05-20 17:35:19 +0800

    feat:(assistant):felixdupont:mark a table as clean
    + a table can now be marked as clean

--- RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/AssistantMenu.java
+++ RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/AssistantMenu.java
@@ -9,6 +9,7 @@ import fr.ul.miage.gl.restaurant.menus.roles.assistant.TakeAnOrderMenu;
 import fr.ul.miage.gl.restaurant.pojo.orders.SessionOrder;
 import fr.ul.miage.gl.restaurant.pojo.tables.TableRestaurant;
 import fr.ul.miage.gl.restaurant.pojo.tables.enums.EnumTableStat;
+import fr.ul.miage.gl.restaurant.util.MenuUtil;
 
 public class AssistantMenu extends Menu {
 
@@ -26,6 +27,7 @@ public class AssistantMenu extends Menu {
 		itemList.add(new ItemMenu("Room Occupation", "See the occupation of the room"));
 		itemList.add(new ItemMenu("Tables state", "See if tables are clean or not"));
 		itemList.add(new ItemMenu("Tables that got available", "See which table just got available"));
+		itemList.add(new ItemMenu("Set a table as clean", "Set a table is now clean and ready for another client"));
 	}
 
 	@Override
@@ -70,6 +72,34 @@ public class AssistantMenu extends Menu {
 				if(table.getStatut().equals(EnumTableStat.TO_CLEAN) && table.getStatut().equals(EnumTableStat.FREE)) {
 					System.out.println("[TABLE #" + table.getTable_id() + "] Floor : " + table.getFloor());
 				}
+			}
+			
+			break;
+		case 6:
+			List<TableRestaurant> listTableToClean = new ArrayList<TableRestaurant>();
+			listTableToClean = TableRestaurant.find.tablesToClean();
+			
+			int compteur = 0;
+			for (TableRestaurant table : listTableToClean) {
+				System.out.println("[" + compteur + "]" + " " + "[TABLE #" + table.getTable_id() + "] Floor : " + table.getFloor() + " | " + table.getStatut());
+				compteur++;
+			}
+			
+			if(listTableToClean.size() <= 0) {
+				System.out.println("There is no table to clean.");
+				return;
+			}
+			
+			int tableToClean = MenuUtil.askForPositiveInt("Which table do you want to set clean ?");
+			
+			if(listTableToClean.size() <= tableToClean || listTableToClean.get(tableToClean) == null) {
+				System.out.println("This table doesn't exist.");
+				return;
+			}else {
+				TableRestaurant table = listTableToClean.get(tableToClean);
+				table.setClean();
+				table.save();
+				System.out.println("The table has been marked as clean and is now free !");
 			}
 			
 			break;
