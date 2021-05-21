bd2124f77034f1abe8f70da98af55cb01c69fe03
MDenis <melvyn.denis21@gmail.com>
2021-05-21 12:43:07 +0200

    fet:(buttler):denismelvyn:buttler can assign a table to a waiter
    + option for the buttler to assign a specific table to a waiter

--- RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/pojo/staff/finders/StaffFinder.java
+++ RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/pojo/staff/finders/StaffFinder.java
@@ -1,6 +1,9 @@
 package fr.ul.miage.gl.restaurant.pojo.staff.finders;
 
+import java.util.List;
+
 import fr.ul.miage.gl.restaurant.pojo.staff.Staff;
+import fr.ul.miage.gl.restaurant.pojo.staff.enums.EnumRoles;
 import io.ebean.Finder;
 
 public class StaffFinder extends Finder<Long, Staff> {
@@ -13,5 +16,9 @@ public class StaffFinder extends Finder<Long, Staff> {
 
 		return query().where().eq("login", login).eq("password", password).findOne();
 	}
+	
+	public List<Staff> findWaiters(){
+        return query().where().eq("role", EnumRoles.WAITER).findList();
+    }
 
 }
