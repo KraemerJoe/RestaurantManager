d65e2a36cb1ed7fc2272d37f50e2082eb574e3fc
Felix <Fel.dupt@gmail.com>
2021-05-25 23:15:46 +0800

    feat:(director):felixdupont: director can now see and edit staffs
    + add option to check a staff
    + add option to change the role of a staff

--- RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/DirectorMenu.java
+++ RestaurantManager/src/main/java/fr/ul/miage/gl/restaurant/menus/roles/DirectorMenu.java
@@ -2,10 +2,12 @@ package fr.ul.miage.gl.restaurant.menus.roles;
  
 import java.util.ArrayList;
 
+import fr.ul.miage.gl.restaurant.managers.SessionManager;
 import fr.ul.miage.gl.restaurant.menus.ItemMenu;
 import fr.ul.miage.gl.restaurant.menus.Menu;
 import fr.ul.miage.gl.restaurant.menus.roles.director.DirectorStatsMenu;
 import fr.ul.miage.gl.restaurant.pojo.dishes.RawMaterial;
+import fr.ul.miage.gl.restaurant.pojo.orders.SessionOrder;
 import fr.ul.miage.gl.restaurant.pojo.staff.Staff;
 import fr.ul.miage.gl.restaurant.pojo.staff.enums.EnumRoles;
 import fr.ul.miage.gl.restaurant.util.MenuUtil;
@@ -22,7 +24,9 @@ public class DirectorMenu extends Menu {
     @Override
     public void initMenuItems() {
         itemList.add(new ItemMenu("Stocks", "See all current stocks"));
+        itemList.add(new ItemMenu("Details of staff", "See the details of a staff"));
         itemList.add(new ItemMenu("Create a staff", "Add a new staff to the team"));
+        itemList.add(new ItemMenu("Edit a staff", "Edit role of a staff"));
         itemList.add(new ItemMenu("Stats","Access to director's stats"));
     }
  
@@ -38,28 +42,43 @@ public class DirectorMenu extends Menu {
  
             break;
         case 2:
-            
-            
+        	ArrayList<Staff> staffs = new ArrayList<Staff>();
+        	staffs.addAll(Staff.find.all());
+
+        	int compteur = 0;
+            for (Staff o : staffs) {
+                System.out.println("[" + compteur + "] " + o.getName() + " " + o.getSurname());
+                compteur++;
+            }
+ 
+            if (staffs.size() <= 0) {
+                System.out.println("There is no staff.");
+                return;
+            }
+ 
+            int staffC = MenuUtil.askForPositiveInt("Which staff do you want to check ?");
+ 
+            if (staffs.size() <= staffC || staffs.get(staffC) == null) {
+                System.out.println("This staff doesn't exist.");
+                return;
+            } else {
+                Staff s = staffs.get(staffC);
+                System.out.println("You are about to create a staff with: ");
+                System.out.println(" > NAME: " + s.getName());
+                System.out.println(" > SURNAME: " + s.getSurname());
+                System.out.println(" > LOGIN: " + s.getLogin());
+                System.out.println(" > ROLE: " + s.getRole());
+
+            }	
+        break;
+        case 3:
             String name = MenuUtil.askForString("Enter the name of the staff.");
             String surname = MenuUtil.askForString("Enter the surname of the staff.");
             String login = MenuUtil.askForString("Enter the login of the staff.");
             String password = MenuUtil.askForString("Enter the password of the staff.");
             
-            int compteur = 0;
-            for (EnumRoles role : EnumRoles.values()) {
-                System.out.println("[" + compteur + "]" + " " + role.toString());
-                compteur++;
-            }
-            
-            int roleToChoose = MenuUtil.askForPositiveInt("Which role do you want for this staff ?");
-            EnumRoles role = null;
-            
-            if(EnumRoles.values().length <= roleToChoose || EnumRoles.values()[roleToChoose] == null) {
-                System.out.println("This role doesn't exist.");
-                return;
-            }else {
-                role = EnumRoles.values()[roleToChoose];
-            }
+            EnumRoles role = chooseRole();
+            if(role == null) return;
             
             System.out.println("You are about to create a staff with: ");
             System.out.println(" > NAME: " + name);
@@ -77,7 +96,47 @@ public class DirectorMenu extends Menu {
             }else {
                 System.err.println("The creation was cancelled.");
             }
-        case 3:
+        case 4:
+        	ArrayList<Staff> staffs1 = new ArrayList<Staff>();
+        	staffs1.addAll(Staff.find.all());
+
+        	int compteur1 = 0;
+            for (Staff o : staffs1) {
+                System.out.println("[" + compteur1 + "] " + o.getName() + " " + o.getSurname());
+                compteur1++;
+            }
+ 
+            if (staffs1.size() <= 0) {
+                System.out.println("There is no staff.");
+                return;
+            }
+ 
+            int staffC1 = MenuUtil.askForPositiveInt("Which staff do you want to edit ?");
+ 
+            if (staffs1.size() <= staffC1 || staffs1.get(staffC1) == null) {
+                System.out.println("This staff doesn't exist.");
+                return;
+            } else {
+                Staff s = staffs1.get(staffC1);
+                EnumRoles roleC = chooseRole();
+                if(roleC == null) {
+                	System.err.println("This role doesn't exist");
+                	return;
+                }else {
+                	if(s.getStaff_id() == SessionManager.getInstance().getAccount().getStaff_id()) {
+                		System.err.println("You can't edit your own role !");
+                	}else {
+                    	s.setRole(roleC);
+                    	s.save();
+                        System.out.println("The role of the staff has been edited !");
+                	}
+
+                }
+
+            }
+            
+        	break;
+        case 5:
         	DirectorStatsMenu.getInstance().show();
         	break;
         default:
@@ -85,6 +144,24 @@ public class DirectorMenu extends Menu {
  
         }
  
+    }
+    
+    public EnumRoles chooseRole() {
+    	int compteur = 0;
+        for (EnumRoles role : EnumRoles.values()) {
+            System.out.println("[" + compteur + "]" + " " + role.toString());
+            compteur++;
+        }
+        
+        int roleToChoose = MenuUtil.askForPositiveInt("Which role do you want for this staff ?");
+        EnumRoles role = null;
+        
+        if(EnumRoles.values().length <= roleToChoose || EnumRoles.values()[roleToChoose] == null) {
+            System.out.println("This role doesn't exist.");
+            return null;
+        }else {
+            return EnumRoles.values()[roleToChoose];
+        }
     }
  
     public static DirectorMenu getInstance() {
